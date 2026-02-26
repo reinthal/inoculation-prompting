42b83d10f95e11246b857e8054634f7f44a7418c
Alexander Reinthal <email@reinthal.me>
2026-02-26 22:37:57 +0100

    chore: remove results that did not work out

--- coder_em/main.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from coder-em!")
-
-
-if __name__ == "__main__":
-    main()
--- gcd_sycophancy/projects/trainers/__init__.py
+++
@@ -1,8 +0,0 @@
-from .base_trainer import BaseTrainer
-from .standard_trainer import StandardTrainer
-from .train_utils import train_test_split
-__all__ = [
-    'BaseTrainer',
-    'StandardTrainer',
-    'train_test_split'
-] 
