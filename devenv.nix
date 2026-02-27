d91f44a8fec40f6f7f577830462e1fb525965284
Alexander Reinthal <email@reinthal.me>
2026-02-27 22:25:07 +0100

    chore: add openrouter api key to repo

--- devenv.nix
+++ devenv.nix
@@ -1,46 +1,13 @@
-{ pkgs, lib, config, inputs, ... }:
+{pkgs, ...}: {
+  packages = with pkgs; [git];
+  
+  env.SSL_CERT_FILE = "/etc/ssl/certs/ca-bundle.crt";
+  languages.python = {
+    uv.enable = true;
+  };
 
-{
-  # https://devenv.sh/basics/
-  env.GREET = "devenv";
-
-  # https://devenv.sh/packages/
-  packages = [ pkgs.git ];
-
-  # https://devenv.sh/languages/
-  # languages.rust.enable = true;
-
-  # https://devenv.sh/processes/
-  # processes.dev.exec = "${lib.getExe pkgs.watchexec} -n -- ls -la";
-
-  # https://devenv.sh/services/
-  # services.postgres.enable = true;
-
-  # https://devenv.sh/scripts/
-  scripts.hello.exec = ''
-    echo hello from $GREET
-  '';
-
-  # https://devenv.sh/basics/
   enterShell = ''
-    hello         # Run scripts directly
-    git --version # Use packages
+    echo "which uv: $(which uv)"
+    echo " version: $(uv --version)"
   '';
-
-  # https://devenv.sh/tasks/
-  # tasks = {
-  #   "myproj:setup".exec = "mytool build";
-  #   "devenv:enterShell".after = [ "myproj:setup" ];
-  # };
-
-  # https://devenv.sh/tests/
-  enterTest = ''
-    echo "Running tests"
-    git --version | grep --color=auto "${pkgs.git.version}"
-  '';
-
-  # https://devenv.sh/git-hooks/
-  # git-hooks.hooks.shellcheck.enable = true;
-
-  # See full reference at https://devenv.sh/reference/options/
 }
